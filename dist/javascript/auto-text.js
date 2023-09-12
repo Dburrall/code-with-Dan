a9515655ed5f4d04e83a3a368316d58a6becacf9
Daniel Burrall <71298259+Dburrall@users.noreply.github.com>
2023-09-11 21:01:58 -0400

    Changed typewriter effect

--- dist/javascript/auto-text.js
+++ dist/javascript/auto-text.js
@@ -1,6 +1,18 @@
-var typed = new Typed(".auto-text",{
-    strings:["HTML", "CSS","Javascript","React"],
-    typeSpeed: 70,
-    backSpeed: 50,
-    loop:true
-})
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('HTML')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('CSS')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('Javascript')
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('or React')
+    .pauseFor(2000)
+    .start();
